77d5c00bb47b65294c8b673482eb2ba3b3cf60d8
Tobias Ivarsson <thobes@gmail.com>
2010-03-23 15:39:06 +0100

    Added support for GraphDatastore CRUD operations.

--- core/src/org/grails/inconsequential/GraphDatastore.java
+++ core/src/org/grails/inconsequential/GraphDatastore.java
@@ -1,7 +1,32 @@
 package org.grails.inconsequential;
 
+import java.util.Map;
+
 /**
  * @author Guillaume Laforge
+ * @author Tobias Ivarsson
  */
-public interface GraphDatastore extends Datastore {
+public interface GraphDatastore<K> extends Datastore
+{
+    Key<K> createRelationship( Context ctxt, Key<K> start, String type,
+            Key<K> end, Map<String, Object> object );
+
+    Iterable<Key<K>> getAllRelationships( Context ctxt, Key<K> node );
+
+    Iterable<Key<K>> getAllRelationships( Context ctxt, Key<K> node,
+            String type, String... moreTypes );
+
+    Iterable<Key<K>> getIncomingRelationships( Context ctxt, Key<K> node );
+
+    Iterable<Key<K>> getIncomingRelationships( Context ctxt, Key<K> node,
+            String type, String... moreTypes );
+
+    Iterable<Key<K>> getOutgoingRelationships( Context ctxt, Key<K> node );
+
+    Iterable<Key<K>> getOutgoingRelationships( Context ctxt, Key<K> node,
+            String type, String... moreTypes );
+
+    Key<K> getStartNode( Context ctxt, Key<K> relationship );
+
+    Key<K> getEndNode( Context ctxt, Key<K> relationship );
 }
